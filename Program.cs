dfa2264aaa5d2830d930069669a292a7cf474bef
Adriana Franco <adrifranco20@hotmail.com>
2021-11-17 13:37:15 -0600

    primer commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
